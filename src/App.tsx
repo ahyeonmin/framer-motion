1b7b1d7c3e77bb2c5e4689550ec1e9ff4b8157e5
AHYEON <yotu00@naver.com>
2023-04-14 02:18:44 +0900

    framer-motion (animate / initial / transition)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -8,18 +9,22 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px;
+  background-color: #fafafa;
+  border-radius: 25px;
+  box-shadow: 0 3px 2px gray, 0 1px 10px;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360}}
+      />
     </Wrapper>
   );
 }

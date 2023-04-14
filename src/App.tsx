364875cc03cd617d3456cc18780ed0b9f80c1047
AHYEON <yotu00@naver.com>
2023-04-14 15:29:27 +0900

    variants (delayChildren / staggerChildren)

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,60 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: #fafafa;
+  background-color: rgba(255, 255, 255, 0.3);
   border-radius: 25px;
-  box-shadow: 0 3px 2px gray, 0 1px 10px;
+  box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
+`;
+const Circle = styled(motion.div)`
+  place-self: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  background-color: #fefefe;
+  box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360,
-    transition: { type: "spring", delay: 0.5
-  }}
+const boxVars = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+}
+const circleVars = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 }
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVars} initial="start" animate="end">
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+      </Box>
     </Wrapper>
   );
 }

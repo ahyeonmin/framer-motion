c6dd324ab69a1c3d6cf7abffff7984e5f12e1c46
AHYEON <yotu00@naver.com>
2023-04-14 23:34:03 +0900

    AnimatePresence / layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -9,46 +9,62 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const BiggerBox = styled(motion.div)`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+`;
+const Box = styled(motion.div)`
+  width: 350px;
+  height: 250px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 15px;
+  box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
+`;
+const Overlay = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 40px;
-  overflow: hidden;
-`;
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 25px;
-  box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `;
 
 const boxVars = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {  }
-}
+  hover: (custom: number) => ({
+    scale: 0.9
+  })
+};
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const [id, setId] = useState<null|string>(null);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragConstraints={biggerBoxRef}
-          dragSnapToOrigin
-          dragElastic={0.5}
-          variants={boxVars}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-        />
-      </BiggerBox>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) =>
+          <Box
+            onClick={() => setId(n)}
+            key={n}
+            layoutId={n}
+            variants={boxVars}
+            custom={n}
+            whileHover="hover"
+            transition={{ type: "spring", duration: 0.5, bounce: 0.3 }}
+          />
+        )}
+      </Grid>
+      <AnimatePresence>
+        { id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={id} style={{ backgroundColor: "#11D086" }}/>
+          </Overlay>
+        ) : null }
+      </AnimatePresence>
     </Wrapper>
   );
 }

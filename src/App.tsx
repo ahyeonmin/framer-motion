1a994112b547b11442bf81f8579393914c1f4ba7
AHYEON <yotu00@naver.com>
2023-04-14 17:03:21 +0900

    whileHover / whileTap / whileDrag

--- src/App.tsx
+++ src/App.tsx
@@ -10,60 +10,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 0.3);
   border-radius: 25px;
   box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
 `;
-const Circle = styled(motion.div)`
-  place-self: center;
-  width: 70px;
-  height: 70px;
-  border-radius: 35px;
-  background-color: #fefefe;
-  box-shadow: 0 3px 2px rgba(255, 255, 255, 0.3), 0 1px 10px;
-`;
 
 const boxVars = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-}
-const circleVars = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "#1dd1a1" }
 }
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <Box drag variants={boxVars} whileHover="hover" whileTap="click" whileDrag="drag"/>
     </Wrapper>
   );
 }
